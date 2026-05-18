b99ed243b960d1e74923d7c31fa5278edcd6fcee
Ishan <ishanmishra756@gmail.com>
2026-05-19 04:34:03 +0530

    first commit

--- frontend/chat_interface.py
+++ frontend/chat_interface.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from backend.analysis import llm
-from langchain.chains import create_history_aware_retriever, create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_classic.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
